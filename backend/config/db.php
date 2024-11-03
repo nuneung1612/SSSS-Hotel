861f703313a011a8ee95b0acec3dfb9631149723
JosephDotGit <65070037@kmitl.ac.th>
2024-11-03 20:08:08 +0700

    change db.php to access AWS

--- backend/config/db.php
+++ backend/config/db.php
@@ -1,10 +1,10 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
+$servername = "database-hotel.c3g80xkhlr8y.us-east-1.rds.amazonaws.com";
+$username = "admin";
+$password = getenv($Pass);
 
 try {
-    $db = new PDO("mysql:host=$servername;dbname=aws", $username, $password);
+    $db = new PDO("mysql:host=$servername;dbname=HotelDB", $username, $password);
     // set the PDO error mode to exception
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
